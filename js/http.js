e9ea8cb7c992be1ae22e53d15257bc26c91085d1
LEORChn <a522237296@gmail.com>
2018-09-19 00:09:34 +0800

    damn HTTPS #2

--- js/http.js
+++ js/http.js
@@ -4,6 +4,8 @@ function http(method,url,formed,dofun,dofail){
 		new XMLHttpRequest();
 	x.timeout=60000;
 	x.responseType="text";
+	if(location.protocol.includes('https'))
+		url=url.replace('^http:','https:');
 	x.open(method.toUpperCase(),url,true);
 	x.onload=dofun;
 	x.ontimeout=x.onerror= dofail? dofail: null;
