ae7b2738b9681330189ab62adde6da9061645ac5
LEORChn <a522237296@gmail.com>
2019-09-29 08:16:36 +0800

    913新布局release

--- home/index.js
+++ home/index.js
@@ -5,18 +5,21 @@
 		for(var i=0; i<jr.length;i++){
 			var j=jr[i],
 				l=fv('blog'),
-				a=ct('div');
+				a=ct('div'),
+				fcn=null; // final class name
 			switch(j[0]){
 				case 0:
 					a.className = 'recent_desc';
 					a.innerHTML = j[3];
 					break;
+				case 3:
+					fcn = 'recent_desc'; // enhanced diary
 				case 1:
 					a.className = 'recent_time';
 					a.innerHTML = j[1];
 					l.appendChild(a);
 					a=ct('div');
-					a.className = 'recent_desc diary';
+					a.className = fcn? fcn: 'recent_desc diary';
 					a.innerHTML = j[3];
 			}
 			l.appendChild(a);
