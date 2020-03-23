ef351a870e0ee3454f54f99990eb9d3f2fae5c7b
LEORChn <a522237296@gmail.com>
2020-03-23 22:05:38 +0800

    http.js 20200320
    现代化，支持更灵活和简便的写法。

--- js/http.js
+++ js/http.js
@@ -1,10 +1,35 @@
-function http(method,url,formed,dofun,dofail,onprogress){
-	var x=window.ActiveXObject?
+function http(){
+	var pointer = 0,
+		method, url, formed, dofun, dofail, onprogress;
+	arr(arguments).forEach(function(e){
+		switch(pointer){
+			case 0:
+				e = e.split(' ', 2); // 允许在第一个参数中用空格将 http-method 与 url 隔开，而省去 引号+逗号+引号 的麻烦
+				method = e[0].toUpperCase();
+				if(e.length > 1){
+					pointer++; // 偏移到下一个
+					e = e[1];
+				}else break;
+			case 1: url = e; break;
+			case 2:
+				if(e instanceof Function){ // 允许不添加 http-body 而直接撰写行为。
+					pointer++; // 偏移到下一个
+				}else{
+					formed = e;
+					break;
+				}
+			case 3: dofun = e; break;
+			case 4: dofail = e; break;
+			case 5: onprogress = e;
+		}
+		pointer++;
+	});
+	var x = 'ActiveXObject' in window?
 		new ActiveXObject("Microsoft.XMLHTTP"):
 		new XMLHttpRequest();
 	if(location.protocol.includes('https'))
-		url=url.replace('^http:','https:');
-	x.open(method.toUpperCase(),url,true);
+		url=url.replace('^http:', 'https:');
+	x.open(method, url, true);
 	x.timeout=60000;
 	x.responseType="text"; // IE要求先open才能设置timeout和responseType
 	x.onload=dofun;
@@ -12,13 +37,30 @@ function http(method,url,formed,dofun,dofail,onprogress){
 	x.onprogress=onprogress;
 	x.send(formed?formed:'');
 }
-function httpj(method, url, formed, dofun, dofail){
-	http(method, url, formed, 
+function httpj(){
+	var a = arr(arguments);
+	if(!a[0].includes(' ')){
+		a[0] += ' ' + a[1];
+		a.splice(1, 1);
+	}
+	if(a[1] instanceof Function) a.splice(1, 0, null); // 如果 method+url 之后不是 String 而是 Function，判断为 onSuccess，将一个 null 插入到 formed
+	while(a.length < 5) a[a.length] = null; // 在剩下的空位填满null
+	// 始终转换为如此格式：0=method+url, 1=formed, 2=onSuccess, 3=onFail, 4=onProgress
+	http(a[0], a[1], // method+url, formed
 		function(){
-			var j = eval('('+ (this.responseText || '{}') +')');
-			if(Array.isArray(j)) j = { stat:this.status, data:j }
-			else j.stat = this.status;
-			dofun(j);
+			var j,
+				stat = this.status,
+				resp = this.responseText;
+			try{
+				j = eval('('+ (resp || '{}') +')');
+			}catch(e){}
+			if(Array.isArray(j)) // JSON Array
+				j = { stat:stat, data:j }
+			else if(j instanceof Object) // JSON Object
+				j.stat = stat;
+			else // Nothing, text/plain
+				j = { stat:stat, data:resp }
+			a[2](j);
 		},
-		dofail);
+		a[3], a[4]); // fail, progress
 }
