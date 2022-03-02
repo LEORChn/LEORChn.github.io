b71d1e75e8eda60732cfd84de24f22c251568fc3
LEORChn <a522237296@gmail.com>
2022-03-02 08:24:12 +0800

    client/bilibili/uploader：日更 v0.3（阶段性成功）
    完成了基本功能，但是代码硬度很高（hard code）

--- workshop/client/bilibili/uploader/lib.js
+++ workshop/client/bilibili/uploader/lib.js
@@ -12,6 +12,16 @@ function BilibiliCookie(cookie){
 		c: 'Cookie: ' + cookie
 	});
 }
+function sign(url){
+	var appkey = /\?.*?appkey=([0-9a-f]*)/.exec(url);
+	var table = {
+		'c1b107428d337928': 'ea85624dfcf12d7cc7b2b3a94fac1f2c',
+		'1d8b6e7d45233436': '560c52ccd288fed045859ed18bffd973'
+	};
+	if(!(appkey && appkey[1] in table)) return url;
+	var params = /\?(.*)/.exec(url)[1].split('&').sort().join('&') + table[appkey[1]];
+	return url + '&sign=' + md5(params);
+}
 var sec_head = `
 sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="98", "Microsoft Edge";v="98"
 sec-ch-ua-mobile: ?0
